a6f7dfba4f69de580164c740db3220100454f64d
alexandere1 <egorov.com@gmail.com>
2013-04-05 20:29:31 +0400

    added layout-xlarge, values-ru

--- AndAplication1/src/com/example/andaplication1/FullscreenActivity1.java
+++ AndAplication1/src/com/example/andaplication1/FullscreenActivity1.java
@@ -4,6 +4,7 @@ import com.example.andaplication1.util.SystemUiHider;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -21,7 +22,7 @@ public class FullscreenActivity1 extends Activity {
 	 * Whether or not the system UI should be auto-hidden after
 	 * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
 	 */
-	private static final boolean AUTO_HIDE = true;
+	private static final boolean AUTO_HIDE = false;
 
 	/**
 	 * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
@@ -38,7 +39,8 @@ public class FullscreenActivity1 extends Activity {
 	/**
 	 * The flags to pass to {@link SystemUiHider#getInstance}.
 	 */
-	private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
+	private static final int HIDER_FLAGS = SystemUiHider.FLAG_LAYOUT_IN_SCREEN_OLDER_DEVICES;
+	//FLAG_HIDE_NAVIGATION
 
 	/**
 	 * The instance of the {@link SystemUiHider} for this activity.
@@ -59,7 +61,7 @@ public class FullscreenActivity1 extends Activity {
 		mSystemUiHider = SystemUiHider.getInstance(this, contentView,
 				HIDER_FLAGS);
 		mSystemUiHider.setup();
-		mSystemUiHider
+		/*mSystemUiHider
 				.setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
 					// Cached values.
 					int mControlsHeight;
@@ -92,13 +94,13 @@ public class FullscreenActivity1 extends Activity {
 									: View.GONE);
 						}
 
-						if (visible && AUTO_HIDE) {
-							// Schedule a hide().
-							delayedHide(AUTO_HIDE_DELAY_MILLIS);
-						}
+						//if (visible && AUTO_HIDE) {
+						//	// Schedule a hide().
+						//	delayedHide(AUTO_HIDE_DELAY_MILLIS);
+						//}
 					}
 				});
-
+*/
 		// Set up the user interaction to manually show or hide the system UI.
 		contentView.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -114,8 +116,7 @@ public class FullscreenActivity1 extends Activity {
 		// Upon interacting with UI controls, delay any scheduled hide()
 		// operations to prevent the jarring behavior of controls going away
 		// while interacting with the UI.
-		findViewById(R.id.dummy_button).setOnTouchListener(
-				mDelayHideTouchListener);
+		//findViewById(R.id.btnExit).setOnTouchListener(mDelayHideTouchListener);
 	}
 
 	@Override
@@ -125,7 +126,7 @@ public class FullscreenActivity1 extends Activity {
 		// Trigger the initial hide() shortly after the activity has been
 		// created, to briefly hint to the user that UI controls
 		// are available.
-		delayedHide(100);
+		//delayedHide(100);
 	}
 
 	/**
@@ -133,7 +134,7 @@ public class FullscreenActivity1 extends Activity {
 	 * system UI. This is to prevent the jarring behavior of controls going away
 	 * while interacting with activity UI.
 	 */
-	View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+	/*View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
 		@Override
 		public boolean onTouch(View view, MotionEvent motionEvent) {
 			if (AUTO_HIDE) {
@@ -141,7 +142,7 @@ public class FullscreenActivity1 extends Activity {
 			}
 			return false;
 		}
-	};
+	};*/
 
 	Handler mHideHandler = new Handler();
 	Runnable mHideRunnable = new Runnable() {
@@ -155,8 +156,18 @@ public class FullscreenActivity1 extends Activity {
 	 * Schedules a call to hide() in [delay] milliseconds, canceling any
 	 * previously scheduled calls.
 	 */
-	private void delayedHide(int delayMillis) {
-		mHideHandler.removeCallbacks(mHideRunnable);
-		mHideHandler.postDelayed(mHideRunnable, delayMillis);
+	//private void delayedHide(int delayMillis) {
+	//	mHideHandler.removeCallbacks(mHideRunnable);
+	//	mHideHandler.postDelayed(mHideRunnable, delayMillis);
+	//}
+	
+	//////////////////////////////////////////////////////////////////////////////////
+	public void btnRegister_onClick(View view){
+		Intent _loginActivity = new Intent(FullscreenActivity1.this, UserData.class);
+		startActivityForResult(_loginActivity, 0);
+	}
+	
+	public void btnExit_onClick(View v){
+		finish();
 	}
 }
